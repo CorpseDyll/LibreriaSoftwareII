8fea74e99e84da7fddbc3ea467d30c302d3c9f81
CorpseDyll <diane.ortegas@uqvirtual.edu.co>
2026-09-22 18:07:09 -0500

    La busqueda quedo lista. Ademas, tambien se agrego mejor animacion a las tarjetas para una interfaz mas bonita.

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,18 +1,42 @@
+function renderBooks(books){
+    const container = document.getElementById("books-container");
+    container.innerHTML = "";
+    if (books.length === 0){
+        container.innerHTML = `
+            <p class="no-results">
+                No se encontraron libros.
+            </p>
+        `;
+        return;
+    }
+    books.forEach(book => {
+       const card = document.createElement("div");
+       card.className = "book-card";
+       card.innerHTML = `
+            <div class="book-info">
+                <h3>${book.title}</h3>
+                <p class="book-author">
+                    ${book.author}
+                </p>
+                <p class="book-genre">
+                    ${book.genre}
+                </p>
+                <p class="book-price">
+                    ${book.price}
+                </p>
+            </div>
+            <button class="details-button">
+                Ver detalles
+            </button>
+        `;
+       container.appendChild(card);
+    });
+}
+
 fetch("/api/books")
     .then(response => response.json())
     .then(books => {
-        const container = document.getElementById("books-container");
-        books.forEach(book => {
-            const card = document.createElement("div");
-            card.className = "book-card";
-            card.innerHTML = `
-                <h3>${book.title}</h3>   
-                <p><strong>Autor:</strong> ${book.author}</p>
-                <p><strong>Género:</strong> ${book.genre}</p>
-                <p><strong>Precio:</strong> ${book.price}</p>
-                `;
-                container.appendChild(card);
-        });
+        renderBooks(books);
     });
 
 const searchInput = document.getElementById("search-input");
@@ -23,6 +47,6 @@ searchButton.addEventListener("click", () => {
     fetch(`/api/books/search?query=${encodeURIComponent(query)}`)
         .then(response => response.json())
         .then(books => {
-            console.log("Resultados:", books);
+            renderBooks(books);
         });
 });
